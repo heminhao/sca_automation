ae9e130847805c12b2c63f9e9a9ad1866fcfc606
Mike <heminhao@hotmail.com>
2017-06-18 15:42:58 +0800

    add opt mod init

--- pkg_sca_autocorr/mod_optimize_adj.py
+++ pkg_sca_autocorr/mod_optimize_adj.py
@@ -9,15 +9,22 @@ import io
 import csv
 import re
 
+def trim_bracket(t_ori_str) :
+    
+    tmp_str = t.ori_str.strip()
+    if (tmp_str[0]!='[') or (tmp_str[-1]!=']') :
+        e.g_exp.raise_exp_data('EI00007')
+    return tmp_str[1:-1]
+
 class ClsOptAdj(object) :
 
     def __init__(self, t_dump_log_id) :
     
         self.m_dump_log_id = t_dump_log_id
-        self.m_engine_str = 'mysql+pymysql://%s:%s@%s/%s?charset=%s' % \
-                (g.g_sca.CfgDatabaseConnection.DatabaseUser,g.g_sca.CfgDatabaseConnection.DatabasePass, \
-                g.g_sca.CfgDatabaseConnection.DatabaseHost,g.g_sca.CfgDatabaseConnection.DatabaseName, \
-                g.g_sca.CfgDatabaseConnection.DatabaseCharset)
+        self.m_engine_str = ('mysql+pymysql://%s:%s@%s/%s?charset=%s' %
+                (g.g_sca.CfgDatabaseConnection.DatabaseUser,g.g_sca.CfgDatabaseConnection.DatabasePass,
+                g.g_sca.CfgDatabaseConnection.DatabaseHost,g.g_sca.CfgDatabaseConnection.DatabaseName,
+                g.g_sca.CfgDatabaseConnection.DatabaseCharset))
         self.m_engine = sy.create_engine(self.m_engine_str)
         self.m_conn = self.m_engine.connect()
         self.m_meta = sy.MetaData()
@@ -31,58 +38,33 @@ class ClsOptAdj(object) :
         self.m_tab_meas_grp_op = sy.Table('sca_grp_op_adj_def', self.m_meta, autoload=True, autoload_with=self.m_engine)
         self.m_tab_meas_delta_val = sy.Table('sca_meas_res_value_delta_def', self.m_meta, autoload=True, autoload_with=self.m_engine)
         
-        tmp_sql = sy.sql.select([self.m_tab_meas_dump_log.c.meas_res_type_id]).where(self.m_tab_meas_dump_log.c.dump_log_id == self.m_dump_log_id)
+        tmp_sql = sy.sql.select([self.m_tab_meas_dump_log]).where(self.m_tab_meas_dump_log.c.dump_log_id == self.m_dump_log_id)
         tmp_rp = self.m_conn.execute(tmp_sql)
-        self.m_meas_res_type_id = tmp_rp.fetchone()
+        tmp_row_res = tmp_rp.first()
+        self.m_meas_res_type_id = tmp_row_res['meas_res_type_id']
+        self.m_tab_data_file = sy.Table(tmp_row_res['full_db_tab_name'], self.m_meta, autoload=True, autoload_with=self.m_engine)
 
     def opt_res_grp(self, t_res_grp_id) :
-    
-    
-    def save_file_todb_csvtab(self, t_file_name) :
-        
-        f = io.open(t_file_name, 'r', encoding='gbk')
-        readerdata = csv.DictReader(f, dialect='excel-tab')
-        max_len = readerdata.fieldnames.__len__() - 2
-        cr_tab_str = 'sy.Table(\'%s\', self.m_meta, ' % self.m_db_tab_name
-        cr_tab_str = cr_tab_str + 'sy.Column(\'dump_log_id\',sy.Integer(),index=True), sy.Column(\'line_num\',sy.BigInteger()), '
-        for i in range(max_len-1) :
-            cr_tab_str = cr_tab_str + 'sy.Column(\'%s\',sy.Text()), ' % readerdata.fieldnames[i]
-        cr_tab_str = cr_tab_str + ' sy.Column(\'%s\',sy.Text()) )' % readerdata.fieldnames[max_len]
-        dyn_tab_obj = eval(cr_tab_str)
-        self.m_meta.create_all(self.m_engine)
-        v_dump_log_id = self.m_conn.execute('select get_seq_nextval(\'' + self.m_seq_dump_log_name + '\') from dual').fetchone()[0]
-        ins_stat = dyn_tab_obj.insert()
-        for rec in readerdata :
-            ins_data = rec
-            ins_data['dump_log_id'] = v_dump_log_id
-            ins_data['line_num'] = readerdata.line_num
-            self.m_conn.execute(ins_stat, ins_data)
-        ins_log_stat = self.m_tab_meas_dump_log.insert()
-        ins_log_data = {}
-        ins_log_data['dump_log_id'] = v_dump_log_id
-        ins_log_data['meas_res_file_grp_id'] = self.m_meas_res_file_grp_id
-        ins_log_data['meas_res_type_id'] = self.m_row_meas_res_grp['meas_res_type_id']
-        ins_log_data['full_file_name'] = t_file_name
-        ins_log_data['full_db_tab_name'] = self.m_db_tab_name
-        self.m_conn.execute(ins_log_stat, ins_log_data)
-        
-    def save_file_todb_others(self, t_file_name) :
-        
-        e.g_exp.raise_exp_data('EN00001')
-        
-    def run_dump(self) :
-        
-        all_files = os.listdir(path = self.m_monitor_dir)
-        # 以tab分隔的带标题行的csv文件格式
-        if self.m_res_format_id == 1 :
-            save_func_name = 'self.save_file_todb_csvtab'
-        # 以逗号分隔的带标题行的csv文件格式（暂未实现）
-        elif self.m_res_format_id == 2 :
-            save_func_name = 'save_file_todb_csvcom'
-        # 其它文件格式（报出异常）
-        else :
-            save_func_name = 'save_file_todb_others'
-        for eachfile in all_files :
-            chk_str = self.m_monitor_file_re.match(eachfile)
-            if chk_str is not None :
-                eval(save_func_name)(self.m_monitor_dir + eachfile)
+
+        tmp_sql = (sy.sql.select([self.m_tab_meas_res_grp_detail])
+                   .where(self.m_tab_meas_res_grp_detail.c.res_grp_id == t_res_grp_id)
+                   .order_by(self.m_tab_meas_res_grp_detail.c.res_grp_detail_id))
+        tmp_bind_param = sy.sql.bindparam('d_key_str',type_=sy.types.String)
+        tmp_alias_sql = (sy.sql.select([self.m_tab_meas_res_field_alias])
+                        .where(sy.and_(
+                                       self.m_tab_meas_res_field_alias.c.meas_res_type_id == self.m_meas_res_type_id,
+                                       sy.or_(
+                                              self.m_tab_meas_res_field_alias.c.key_field_data == tmp_bind_param,
+                                              self.m_tab_meas_res_field_alias.c.key_field_alias == tmp_bind_param)))
+                        )
+        tmp_rp = self.m_conn.execute(tmp_sql)
+        meas_grp_var_dict = {}
+        for tmp_rec in tmp_rp :
+            tmp_sig_row = {}
+            tmp_sig_rp = self.m_conn.execute(tmp_alias_sql, d_key_str=trim_bracket(tmp_rec.key_field_item))
+            tmp_alias_row = tmp_sig_rp.first()
+            tmp_sig_row['key_field_alias'] = tmp_alias_row['key_field_alias']
+            tmp_sig_row['key_field_data'] = tmp_alias_row['key_field_data']
+            tmp_sig_row['key_field_opt_weight'] = tmp_rec.key_field_opt_weight
+            meas_grp_var_dict[tmp_rec.res_grp_detail_id] = tmp_sig_row
+        return meas_grp_var_dict
